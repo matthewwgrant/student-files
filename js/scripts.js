53b3c50a3c470167ba17ce74547304e16fbbdc09
Matthew Grant <matthewgrant.webdev@gmail.com>
2021-10-13 15:59:32 -0400

    Created modal function
    Wrote function to generate modal html. Function does not do anything as of yet, just has the html filled in with the dynamic variables

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,29 @@ function generateEmployees(data) {
 	);
 	gallery.insertAdjacentHTML('beforeend', employees);
 }
+
+function generateModal(data) {
+	const modal = `
+		 <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${data.picture.medium}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">name</h3>
+                    <p class="modal-text">${data.email}</p>
+                    <p class="modal-text cap">${data.city}</p>
+                    <hr>
+                    <p class="modal-text">(555) 555-5555</p>
+                    <p class="modal-text">${data.location.street}, ${data.location.city}, ${data.location.state} ${data.location.postcode}</p>
+                    <p class="modal-text">Birthday: ${data.dob.date}</p>
+                </div>
+            </div>
+
+            // IMPORTANT: Below is only for exceeds tasks 
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>
+        </div>
+	`
+}
